fec11277e21fb1efb45ddc28d6cae5d482cfe180
AmeerHamzaSajjad <ameerhamzasajjad55@gmail.com>
2020-12-28 12:48:29 -0800

    add dinner.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Dinner from './dinner.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Hello World from Ameer</h1>
     </div>
   );
 }
---
+++ src/dinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Dinner(){
+    return(
+        <div>
+            <h1>Today we are serving baryani</h1>
+            <h1>Today we are serving chicken kharai</h1>
+        </div>
+    );
+}
+export default Dinner;
